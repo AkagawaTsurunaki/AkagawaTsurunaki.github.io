e93e2f76feebb7f33dd91007f6e2453732287b4a
AkagawaTsurunaki <965145619@qq.com>
2025-10-24 16:20:18 +0800

    feat: Auto adapt fonts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import router from '@/scripts/router';
 
 import { createApp } from 'vue'
 import App from '@/App.vue'
+import 'amfe-flexible'; // 自动调整字体
 
 const app = createApp(App)
 app.use(router);
