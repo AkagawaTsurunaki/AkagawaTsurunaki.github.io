b260ce10a95dda8b3d9991186a2d9017785b4a09
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-11-06 12:49:29 +0800

    Updates

---
+++ blogRegister.js
@@ -0,0 +1,82 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getBlogItemList = getBlogItemList;
+function getBlogItemList() {
+    return [
+        {
+            mdFilePath: 'blogs/ai-methods/mamba-reproduction.md',
+            tags: ['人工智能', '论文复现'],
+            time: '2025-11-04 18:52',
+        },
+        {
+            mdFilePath: 'blogs/solutions/mmd-unity-solution.md',
+            tags: ['解决方案'],
+            time: '2025-11-02 19:45',
+        },
+        {
+            mdFilePath: 'blogs/solutions/mikumikudance-wont-run-parallel-configuration-incorrect-solution.md',
+            tags: ['解决方案'],
+            time: '2025-10-24 11:48',
+        },
+        {
+            mdFilePath: 'blogs/solutions/win2ubuntu-vnc.md',
+            tags: ['解决方案'],
+            time: '2025-08-15 20:21',
+        },
+        {
+            mdFilePath: 'blogs/solutions/solve-python-auto-import-issues-in-vscode.md',
+            tags: ['解决方案'],
+            time: '2025-09-03 10:07',
+        },
+        {
+            mdFilePath: 'blogs/math-proof/p.0.10-mathematical-induction-in-polynomial-proof.md',
+            tags: ['数学'],
+            time: '2025-10-18 23:30',
+        },
+        {
+            mdFilePath: 'blogs/math-proof/why-scaled-dot-product-attention-formula-divide-by-sqrt-dk.md',
+            tags: ['数学'],
+            time: '2025-10-23 15:06',
+        },
+        {
+            mdFilePath: 'blogs/math-proof/why-population-variance-not-equal-biased-sample-variance.md',
+            tags: ['数学'],
+            time: '2025-05-14 22:29',
+        },
+        {
+            mdFilePath: 'blogs/math-proof/derivation-of-laplace-operator-in-spherical-coordinates.md',
+            tags: ['数学'],
+            time: '2024-11-01 13:03',
+        },
+        {
+            mdFilePath: 'blogs/math-proof/derivation-of-spherical-harmonics.md',
+            tags: ['数学'],
+            time: '2024-12-12 15:53',
+        },
+        {
+            mdFilePath: 'blogs/math-proof/derivation-of-nerf-formula.md',
+            tags: ['数学', '人工智能', 'NeRF'],
+            time: '2024-10-15 14:37',
+        },
+        {
+            mdFilePath: 'blogs/ai-methods/derivation-of-hodgkin-huxley-model.md',
+            tags: ['人工智能'],
+            time: '2024-09-12 19:40',
+        },
+        {
+            mdFilePath: 'blogs/math-proof/meaning-of-energy-function-in-hopfield-neural-network-and-proof-of-delta-e-leq-0.md',
+            tags: ['数学', '人工智能'],
+            time: '2024-10-17 19:53',
+        },
+        {
+            mdFilePath: 'blogs/ai-methods/competitive-learning-in-artificial-neural-networks.md',
+            tags: ['人工智能'],
+            time: '2024-10-24 15:47',
+        },
+        {
+            mdFilePath: 'blogs/ai-methods/dynamics-of-spatiotemporal-behavior-in-neuronal-networks-introduction-reading-notes.md',
+            tags: ['人工智能'],
+            time: '2024-10-24 15:51',
+        },
+    ];
+}
---
+++ markdownUtil.js
@@ -0,0 +1,55 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getMarkdownFileInfoByPath = getMarkdownFileInfoByPath;
+exports.getMarkdownFileInfo = getMarkdownFileInfo;
+async function getMarkdownFileInfoByPath(publicPath, maxLength = 200) {
+    const res = await fetch(publicPath);
+    if (!res.ok)
+        return null;
+    const text = await res.text();
+    return getMarkdownFileInfo(text, maxLength);
+}
+function getMarkdownFileInfo(text, maxLength = 200) {
+    try {
+        // If we get the title of such a markdown file, then return the other lines below the title.
+        // Else we just return all preview content if we can not find the title.
+        const index = getIndexOfFirstH1(text);
+        if (index < 0) {
+            return { title: null, preview: text.slice(0, Math.max(0, maxLength)) + '...' };
+        }
+        const preview = text.slice(index, index + Math.max(0, maxLength));
+        const indexOfFirstBr = getIndexOfFirstBr(preview);
+        if (indexOfFirstBr > 0) {
+            return {
+                title: preview.slice(2, indexOfFirstBr + 1),
+                preview: preview.slice(indexOfFirstBr + 1, indexOfFirstBr + 1 + Math.max(0, preview.length)) +
+                    '...',
+            };
+        }
+        // Only have title but no more content
+        return {
+            title: null,
+            preview: preview.slice(2, 2 + Math.max(0, preview.length)) + '...',
+        };
+    }
+    catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+function getIndexOfFirstH1(text) {
+    for (let index = 0; index < text.length - 1; index++) {
+        if (text[index] == '#' && text[index + 1] == ' ') {
+            return index;
+        }
+    }
+    return -1;
+}
+function getIndexOfFirstBr(str) {
+    for (let index = 0; index < str.length; index++) {
+        if (str[index] == '\n') {
+            return index;
+        }
+    }
+    return -1;
+}
