83e0e6ece069c2fedee02660afd08d9c0ea1fbbc
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2026-09-01 09:18:26 +0800

    feat: Add bonus for TryCatchException music

--- src/views/bonus/PanicReport.vue
+++ src/views/bonus/PanicReport.vue
@@ -1,18 +1,40 @@
 <template>
     <div>Error</div>
+    <div>
+        <pre>?
+            / \
+            / \
+            / /\|\ \
+            \ O O /
+            \ v /
+            ---
+        </pre>
+
+        <pre>
+            -
+            | |
+            | |
+            |_|
+            _
+            (_)
+        </pre>
+    </div>
+
     <audio ref="panicAudio" src="/music/[重音テト]不知道在唱些什么的铁头酱.wav" loop></audio>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
-
-const panicAudio = ref(null)
+const panicAudio = ref<any>(null)
 
 onMounted(() => {
-    panicAudio.value.play().catch(() => {
-        document.addEventListener('click', () => {
-            panicAudio.value?.play()
-        }, { once: true })
-    })
+    if (panicAudio && panicAudio.value) {
+        panicAudio.value.play().catch(() => {
+            document.addEventListener('click', () => {
+                panicAudio.value?.play()
+            }, { once: true })
+        })
+    }
+
 })
 </script>
